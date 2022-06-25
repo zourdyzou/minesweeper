d97161d6705db4ab10e780072af407a36c91579d
Muhammad Zourdy <zhafarmuhammad12@gmail.com>
2022-06-25 15:45:02 +0200

    Modify the react starter file

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,33 @@
 import React, { FunctionComponent } from "react";
+import { PropsWithChildrenOnly } from "./react";
+
 
 export const App: FunctionComponent = () => {
   return (
     <div className="App-container">
-      <p>
-        Edit <code>src/App.tsx</code> and save to reload the browser
-      </p>
+      <Header>
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload the browser
+        </p>
+      </Header>
     </div>
   );
 };
 
+interface HeaderProps {
+  logo?: string;
+}
 
+const Header: FunctionComponent<PropsWithChildrenOnly & HeaderProps> = ({
+  children,
+  logo,
+}) => (
+  <header className="App-header">
+    {logo ? (
+      <img src={logo} className="App-logo" alt="logo" />
+    ) : (
+      "There is no any logo!"
+    )}
+    {children}
+  </header>
+);
