e05c92f2e5f9fdb3bbc1f613c299cc2dd4320b64
Muhammad Zourdy <zhafarmuhammad12@gmail.com>
2022-07-03 02:27:00 +0200

    Testing the field grid logic

--- src/components/field/field.test.ts
+++ src/components/field/field.test.ts
@@ -1,18 +1,18 @@
-import { CellState, emptyFieldGenerator } from "./field";
+import { CellState, fieldGenerator, generateFieldWithDefaultState } from "./field";
 
 const { empty, bomb, hidden } = CellState;
 
 describe("Field Generator", () => {
   describe("emptyFieldGenerator tests", () => {
     it("should go with 2x2", function () {
-      expect(emptyFieldGenerator(2)).toStrictEqual([
+      expect(generateFieldWithDefaultState(2)).toStrictEqual([
         [empty, empty],
         [empty, empty],
       ]);
     });
 
     it("should go with 3x3", function () {
-      expect(emptyFieldGenerator(3)).toStrictEqual([
+      expect(generateFieldWithDefaultState(3)).toStrictEqual([
         [empty, empty, empty],
         [empty, empty, empty],
         [empty, empty, empty],
@@ -20,11 +20,73 @@ describe("Field Generator", () => {
     });
 
     it("should go with 2x2", function () {
-      expect(emptyFieldGenerator(3, hidden)).toStrictEqual([
+      expect(generateFieldWithDefaultState(3, hidden)).toStrictEqual([
         [hidden, hidden, hidden],
         [hidden, hidden, hidden],
         [hidden, hidden, hidden],
       ]);
+    });
+  });
+
+  describe("Simple Case", () => {
+    it("should throw wrong density", function () {
+      const errorText = "Probability must be between 0 and 1";
+      expect(() => fieldGenerator(1, -1)).toThrow(errorText);
+      expect(() => fieldGenerator(1, 2)).toThrow(errorText);
+    });
+
+    it("should have a smallest possible field without mine(bomb)", function () {
+      expect(fieldGenerator(1, 0)).toStrictEqual([[empty]]);
+    });
+
+    it("should have a BIG field without mine", () => {
+      expect(fieldGenerator(10, 0)).toStrictEqual([
+        [empty, empty, empty, empty, empty, empty, empty, empty, empty, empty],
+        [empty, empty, empty, empty, empty, empty, empty, empty, empty, empty],
+        [empty, empty, empty, empty, empty, empty, empty, empty, empty, empty],
+        [empty, empty, empty, empty, empty, empty, empty, empty, empty, empty],
+        [empty, empty, empty, empty, empty, empty, empty, empty, empty, empty],
+        [empty, empty, empty, empty, empty, empty, empty, empty, empty, empty],
+        [empty, empty, empty, empty, empty, empty, empty, empty, empty, empty],
+        [empty, empty, empty, empty, empty, empty, empty, empty, empty, empty],
+        [empty, empty, empty, empty, empty, empty, empty, empty, empty, empty],
+        [empty, empty, empty, empty, empty, empty, empty, empty, empty, empty],
+      ]);
+    });
+
+    it("should have a smallest possible field with mine(bomb)", function () {
+      expect(fieldGenerator(1, 1)).toStrictEqual([[bomb]]);
+    });
+
+    it("should have 2x2 field with mines(bomb)", function () {
+      expect(fieldGenerator(2, 1)).toStrictEqual([
+        [bomb, bomb],
+        [bomb, bomb],
+      ]);
+    });
+
+    it("should have 2x2 field with 50% probability mines(bomb)", function () {
+      expect(fieldGenerator(2, 0.5)).toStrictEqual([
+        [bomb, bomb],
+        [empty, empty],
+      ]);
+    });
+
+    it("should have 4x4 field with 50% of probability mines(bomb)", function () {
+      expect(fieldGenerator(4, 0.5)).toStrictEqual([
+        [bomb, bomb, bomb, bomb],
+        [bomb, bomb, bomb, bomb],
+        [empty, empty, empty, empty],
+        [empty, empty, empty, empty],
+      ]);
+    });
+
+    it("should have 3x3 field with 50% of probability mines(bomb)", function () {
+      const field = fieldGenerator(3, 0.5);
+      const flatField = field.flat();
+
+      console.table(field);
+      console.table(flatField);
     });
   });
 });
--- src/components/field/field.ts
+++ src/components/field/field.ts
@@ -1,3 +1,5 @@
+import { resolve } from "path";
+
 export type Cell = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
 export type Field = Cell[][];
 export type Coordinates = [number, number];
@@ -10,5 +12,33 @@ export const CellState: Record<string, Cell> = {
   weakMark: 12,
 };
 
-export const emptyFieldGenerator = (size: number, state: Cell = CellState.empty): Field =>
+export const generateFieldWithDefaultState = (size: number, state: Cell = CellState.empty): Field =>
   new Array(size).fill(null).map(() => new Array(size).fill(state));
+
+export const fieldGenerator = (size: number, probability: number): Field => {
+  if (probability < 0 || probability > 1) {
+    throw new Error("Probability must be between 0 and 1");
+  }
+
+  const result: Field = generateFieldWithDefaultState(size);
+
+  // the size grid of the cells
+  let unprocessedCells = size * size;
+  // cells with bombs included
+  let restCellsWithBombs = unprocessedCells * probability;
+
+  for (let y = 0; y < size; y++) {
+    for (let x = 0; x < size; x++) {
+      if (restCellsWithBombs === 0) {
+        return result;
+      }
+      if (restCellsWithBombs / unprocessedCells > 0) {
+        result[y][x] = CellState.bomb;
+        restCellsWithBombs--;
+      }
+      unprocessedCells--;
+    }
+  }
+
+  return result;
+};
