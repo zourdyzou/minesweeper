4f7264564894dfd0747f82af6501cbc60fd21765
Muhammad Zourdy <zhafarmuhammad12@gmail.com>
2022-07-31 02:54:32 +0200

    Refactor mockData field

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import "normalize.css/normalize.css";
 
 import { GridComponent } from "@components/grid";
 import { Field } from "@helpers/field";
+import { MockFieldData } from "@helpers/mockData";
 import React from "react";
 import { createRoot } from "react-dom/client";
 
@@ -15,21 +16,6 @@ import { createRoot } from "react-dom/client";
 
 const container = document.getElementById("root") as HTMLElement;
 const root = createRoot(container);
-
-const MockFieldData: Field = [
-  [9, 2, 9, 1, 0, 0, 1, 1, 1, 1, 1, 1],
-  [1, 2, 2, 2, 1, 0, 1, 9, 1, 1, 9, 1],
-  [0, 0, 1, 9, 10, 0, 2, 2, 2, 1, 1, 1],
-  [0, 0, 10, 10, 1, 0, 1, 9, 1, 2, 2, 2],
-  [0, 1, 1, 2, 2, 9, 1, 1, 1, 0, 0, 0],
-  [0, 1, 9, 3, 9, 2, 10, 0, 0, 2, 1, 1],
-  [0, 2, 2, 4, 9, 2, 10, 1, 1, 1, 9, 1],
-  [0, 1, 9, 2, 1, 1, 1, 9, 1, 2, 2, 2],
-  [0, 1, 10, 10, 0, 0, 1, 1, 1, 1, 9, 1],
-  [0, 1, 10, 10, 0, 0, 1, 1, 1, 1, 9, 1],
-  [0, 1, 1, 1, 0, 0, 1, 1, 1, 1, 9, 1],
-  [0, 1, 1, 1, 0, 0, 1, 1, 1, 1, 9, 1],
-];
 
 root.render(
   <React.StrictMode>
