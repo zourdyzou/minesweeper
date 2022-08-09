1e991f369313b01b5067f8aacb421fb789740302
Muhammad Zourdy <zhafarmuhammad12@gmail.com>
2022-08-10 00:46:41 +0200

    Add new test for use-game hook

--- src/modules/GameWithHooks/hooks/use-game/use-game.test.tsx
+++ src/modules/GameWithHooks/hooks/use-game/use-game.test.tsx
@@ -1,8 +1,11 @@
+import { Field } from "@helpers/field";
 import { GameLevels, GameSettings } from "@modules/GameSettings";
 import { useGame } from "@modules/GameWithHooks/hooks/use-game/index";
-import { renderHook } from "@testing-library/react";
+import { act, renderHook } from "@testing-library/react";
 
-const [beginner, ,] = GameLevels;
+const [beginner, intermediate, expert] = GameLevels;
+
+const flatWithFilter = (field: Field, cond: number) => field.flat().filter((v) => v === cond);
 
 describe("useGame test cases", function () {
   it("should render hooks behaviour", function () {
@@ -16,5 +19,20 @@ describe("useGame test cases", function () {
       settings: GameSettings.beginner,
     });
     expect(playerField).toHaveLength(9);
+  });
+
+  it("should change level using onChangeGameHandler", function () {
+    const { result } = renderHook(useGame);
+    const { playerField: beginnerPlayerField, onChangeLevelHandler } = result.current;
+
+    expect(beginnerPlayerField).toHaveLength(9);
+
+    act(() => onChangeLevelHandler(intermediate));
+    const { playerField: intermediatePlayerField } = result.current;
+    expect(intermediatePlayerField).toHaveLength(16);
+
+    act(() => onChangeLevelHandler(expert));
+    const { playerField: expertPlayerField } = result.current;
+    expect(expertPlayerField).toHaveLength(22);
   });
 });
